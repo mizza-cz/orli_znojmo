04d9f176fff5917d240d6dc6eec6d4f518f04ad1
mizza-cz <mizza.cz666@gmail.com>
2022-12-01 20:58:46 +0100

    start

---
+++ src/components/header/header.js
@@ -0,0 +1,28 @@
+headerNavOpenerClick();
+function headerNavOpenerClick() {
+  const bodyEl = document.querySelector("body");
+  const headerNavOpener = document.querySelector(".js-header__opener");
+  if (!bodyEl || !headerNavOpener) {
+    return;
+  }
+  headerNavOpener.addEventListener("click", function () {
+    if (!this.classList.contains("is-open")) {
+      bodyEl.classList.add("is-nav-open");
+      this.classList.add("is-open");
+    } else {
+      bodyEl.classList.remove("is-nav-open");
+      this.classList.remove("is-open");
+    }
+  });
+}
+
+var e,
+  t = window.location.hash;
+"" != t &&
+  (e = $("a[name='" + t + "']")).length &&
+  $("html,body").animate({ scrollTop: e.offset().top }, "slow"),
+  $(window).bind("scroll", function () {
+    64 < $(window).scrollTop()
+      ? $(".menu-container").addClass("fixed")
+      : $(".menu-container").removeClass("fixed");
+  });
